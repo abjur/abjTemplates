123a7b396b238e0d5416223cc14d9cccf50e1930
Fernando Corrêa <fptcorrea@gmail.com>
2017-09-14 18:56:31 -0300

    updates pdf_document

--- R/pdf_document.R
+++ R/pdf_document.R
@@ -1,8 +1,15 @@
 #' @export
 pdf_document <- function(...) {
-  inherit_pdf_document2(includes = list(
+  base <- inherit_pdf_document2(includes = list(
     in_header = find_resource('pdf_document', 'style.sty'),
     before_body = find_resource('pdf_document', 'body.tex')),
     template = find_resource("pdf_document", "template.tex"),
     ...)
+
+  base$knitr$opts_chunk$comment <- F
+  base$knitr$opts_chunk$message <- F
+
+  base$knitr$opts_chunk$fig.pos <- "H"
+
+  base
 }
